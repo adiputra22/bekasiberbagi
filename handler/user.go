8bc2ddcb26f24ef5bd4781847b989e737984e86e
Adiputra <tuts.adiputra@gmail.com>
2021-08-08 10:57:25 +0700

    login endpoint

--- handler/user.go
+++ handler/user.go
@@ -42,3 +42,29 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		response := response.APIResponseValidationFailed("Login failed", http.StatusUnprocessableEntity, err)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedUser, err := h.userService.Login(input)
+
+	if err != nil {
+		response := response.APIResponseFailed(err.Error(), http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedUser, "token")
+
+	response := response.APIResponseSuccess("Success login", http.StatusOK, formatter)
+
+	c.JSON(http.StatusOK, response)
+}
--- user/service.go
+++ user/service.go
@@ -1,11 +1,14 @@
 package user
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
+	Login(input LoginInput) (User, error)
 }
 
 type service struct {
@@ -36,4 +39,27 @@ func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
 	}
 
 	return newUser, nil
+}
+
+func (s *service) Login(input LoginInput) (User, error) {
+	email := input.Email
+	password := input.Password
+
+	user, err := s.repository.FindByEmail(email)
+
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("User not found")
+	}
+
+	errPassword := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+
+	if errPassword != nil {
+		return user, errPassword
+	}
+
+	return user, nil
 }
