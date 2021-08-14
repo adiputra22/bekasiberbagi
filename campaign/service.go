201674b56539ff8694d519a8261fa38e710b0dac
Adiputra <tuts.adiputra@gmail.com>
2021-08-14 13:10:57 +0700

    campaign service

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,27 @@
 package campaign
+
+type Service interface {
+	FindCampaigns(userId int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository: repository}
+}
+
+func (s *service) FindCampaigns(userId int) ([]Campaign, error) {
+	campaigns, err := s.repository.FindAll()
+
+	if userId != 0 {
+		campaigns, err = s.repository.FindByUserID(userId)
+	}
+
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
