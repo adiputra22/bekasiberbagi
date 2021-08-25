7024ec3d821c996521a9db6e798c9dd10163fe20
Adiputra <tuts.adiputra@gmail.com>
2021-08-25 21:53:18 +0700

    transactions of campaign endpoint

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,43 @@
 package handler
+
+import (
+	"bekasiberbagi/response"
+	"bekasiberbagi/transaction"
+	"bekasiberbagi/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransaction(c *gin.Context) {
+	var input transaction.GetCampaignTransactionInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := response.APIResponseFailed("Error uri", http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	input.User = currentUser
+
+	transactions, err := h.service.GetTransactionByCampaignId(input)
+	if err != nil {
+		response := response.APIResponseFailed(err.Error(), http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := response.APIResponseSuccess("Campaign Transactions", http.StatusOK, transaction.FormatCampaignTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+}
--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,39 @@
 package transaction
+
+import (
+	"bekasiberbagi/campaign"
+	"errors"
+)
+
+type service struct {
+	repository         Repository
+	campaignRepository campaign.Repository
+}
+
+type Service interface {
+	GetTransactionByCampaignId(input GetCampaignTransactionInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
+}
+
+func (s *service) GetTransactionByCampaignId(input GetCampaignTransactionInput) ([]Transaction, error) {
+	campaign, err := s.campaignRepository.FindById(input.ID)
+
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("NOT AUTHORIZATION OF THIS ITEM")
+	}
+
+	transactions, err := s.repository.GetByCampaignId(input.ID)
+
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
